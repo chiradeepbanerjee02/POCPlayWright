7613b5fdc9c05c3e425e1899d57f8993106ef07f
Chiradeep Banerjee <chiradeep.banerjee@hyland.com>
2026-02-03 12:25:26 -0600

    Enhance search validation test with date selection clear

--- POC-PW/tests/test_SearchValidation.py
+++ POC-PW/tests/test_SearchValidation.py
@@ -2,14 +2,23 @@ from playwright.sync_api import expect
 
 def test_SearchValidation(page):
        page.goto("http://localhost:93")
-       button = page.get_by_role("button", name="Search", exact=True)
-       expect(button).to_be_visible()
-       expect(button).to_be_enabled()
-       button.click()
+       startdatebutton= page.locator("xpath=//*[@id='cdk-accordion-child-0']//mat-form-field[.//mat-label[normalize-space()='Event start date']]//button[@matsuffix and @aria-label='Select date']")
+       expect(startdatebutton).to_be_visible()
+       expect(startdatebutton).to_be_enabled()
+       startdatebutton.click()
+       clearbutton=page.locator("xpath=//mat-dialog-container//app-lookup-date//div[contains(@class,'mat-dialog-actions') or self::mat-dialog-actions]//button[normalize-space()='Clear']")
+       expect(clearbutton).to_be_visible()
+       expect(clearbutton).to_be_enabled()
+       clearbutton.click()
+       searchbutton = page.locator("xpath=(//*[@id='cdk-accordion-child-0']//button)[last()-0]")
+       expect(searchbutton).to_be_visible()
+       expect(searchbutton).to_be_enabled()
+       searchbutton.click()
        print("Validate that the Search Result is displayed")
        el = page.locator("xpath=//cdk-virtual-scroll-viewport//table")
        expect(el).to_have_count(1)     # exists in DOM
        # or, if it must be visible:
        expect(el).to_be_visible()
+
 
 
